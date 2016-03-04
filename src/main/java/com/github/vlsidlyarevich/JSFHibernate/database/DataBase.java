af040df802b7d6fac3398f4f35fb35f45aad940d
Vladislav Sidlyarevich <vlsidlyarevich@gmail.com>
2016-03-04 17:56:01 +0300

    Initial commit Project created

---
+++ src/main/java/com/github/vlsidlyarevich/JSFHibernate/database/DataBase.java
@@ -0,0 +1,17 @@
+package com.github.vlsidlyarevich.JSFHibernate.database;
+
+
+import java.sql.Connection;
+
+public class DataBase {
+
+
+    public static void connect(){
+
+        Connection connection;
+        
+    }
+
+
+
+}
---
+++ src/main/resources/test.sql
@@ -0,0 +1,1 @@
+insert into users (name,role) values('Genek','ROLE_USER');
